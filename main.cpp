cac875849101926f26ae263497eb3c2fefe44051
nopacak <nopacak315@gmail.com>
2024-07-06 21:26:50 +0200

    Changes to be committed: 	new file:   .gitignore 	new file:   main.cpp
    Changes not staged for commit:
    	modified:   main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <cppunit/TestCase.h>
+
+class BasicTest : public CppUnit::TestCase {
+    public:
+    BasicTest(std::string name) : CppUnit::TestCase(name) {};
+
+    void runTest() override
+    {
+        CPPUNIT_ASSERT(2+2==4);
+        CPPUNIT_ASSERT(2+2==3);
+    }
+};
+
+int main()
+{
+    BasicTest test{"BasicTest"};
+    test.runTest();
+}
